69074b30eecdd07bba1543517447f07e2822c1f6
Francis <79401136+Francis-CyAI@users.noreply.github.com>
2025-04-18 13:27:59 +0000

    Completed react lab 5 assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Welcome name="Francis Kalunga" />
     </div>
+  );
+}
+
+function Welcome({ name }) {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <p>{show ? `Welcome ${name}` : ""}</p>
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        toggle
+      </button>
+    </>
   );
 }
